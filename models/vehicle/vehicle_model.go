847a913e8a457abc5d26ceacac99b5a5ddec5a9d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-05-17 22:34:06 +0700

    add vehicle model

---
+++ models/vehicle/vehicle_model.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type VehicleModel struct {
+	Id             uint      `json:"id" gorm:"primary_key"`
+	UserId         uint      `json:"user_id"`
+	VehicleName    string    `json:"vehicle_name"`
+	VehicleImage   string    `json:"vehicle_image"`
+	Year           string    `json:"year"`
+	EngineCapacity string    `json:"engine_capacity"`
+	TankCapacity   string    `json:"tank_capacity"`
+	Color          string    `json:"color"`
+	MachineNumber  string    `json:"machine_number"`
+	ChassisNumber  string    `json:"chassis_number"`
+	CreatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}

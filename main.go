fd6eb0d3a3b635f4712d881a6574c53385a4eff5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-05-20 22:53:14 +0700

    update main go

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	setup "project_vehicle_log_backend/database"
+	account "project_vehicle_log_backend/models/account"
 	routes "project_vehicle_log_backend/routes"
 )
 
 func main() {
 	db := setup.SetupDB()
 	sqlDB := db.DB()
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	sqlDB.SetMaxIdleConns(10)
 
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetMaxOpenConns(100)
-	// db.AutoMigrate(&account.AccountUserV2{}, &event.Event{}, &event.VolunteersForm{}, &notif.Notification{})
+	db.AutoMigrate(&account.AccountUserModel{})
 
 	r := routes.SetupRoutes(db)
 	err := r.Run(":8080")

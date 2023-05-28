dbe83ff989f64c1bef896193bb86c4d45680aa0b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-05-28 22:46:25 +0700

    update vehicle controller: add get all & create log func

--- controllers/vehiclecontroller.go
+++ controllers/vehiclecontroller.go
@@ -105,3 +105,140 @@ func CreateVehicle(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, createVehicleResponse)
 }
+
+// type VehicleData struct {
+// 	UserId                    uint                                 `gorm:"not null" json:"user_id" validate:"required"`
+// 	VehicleName               string                               `gorm:"not null" json:"vehicle_name" validate:"required"`
+// 	VehicleImage              string                               `gorm:"not null" json:"vehicle_image" validate:"required"`
+// 	Year                      string                               `gorm:"not null" json:"year" validate:"required"`
+// 	EngineCapacity            string                               `gorm:"not null" json:"engine_capacity" validate:"required"`
+// 	TankCapacity              string                               `gorm:"not null" json:"tank_capacity" validate:"required"`
+// 	Color                     string                               `gorm:"not null" json:"color" validate:"required"`
+// 	MachineNumber             string                               `gorm:"not null" json:"machine_number" validate:"required"`
+// 	ChassisNumber             string                               `gorm:"not null" json:"chassis_number" validate:"required"`
+// 	VehicleMeasurementLogData []vehicle.VehicleMeasurementLogModel `gorm:"not null" json:"vehicle_measurement_log_data" validate:"required"`
+// }
+
+type GetAllEventResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []vehicle.VehicleModel
+}
+
+func GetAllVehicleData(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+	var vehicleData []vehicle.VehicleModel
+
+	result := db.Find(&vehicleData)
+
+	if result.Value == nil {
+		log.Println(fmt.Sprintf("error log3: %s", result.Error))
+		c.JSON(http.StatusNotFound, GetAllEventResponse{
+			Status:  404,
+			Message: "get all event Failed",
+			Data:    []vehicle.VehicleModel{},
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, GetAllEventResponse{
+		Status:  200,
+		Message: "get all event success",
+		Data:    vehicleData,
+	})
+}
+
+type CreateLogVehicleRequest struct {
+	UserId              uint   `json:"user_id"`
+	VehicleId           uint   `json:"vehicle_id"`
+	MeasurementTitle    string `json:"measurement_title"`
+	CurrentOdo          string `json:"current_odd"`
+	EstimateOdoChanging string `json:"estimate_odo_changing"`
+	AmountExpenses      string `json:"amount_expenses"`
+	CheckpointDate      string `json:"checkpoint_date"`
+	Notes               string `json:"notes"`
+}
+type CreateLogVehicleResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
+func CreateLogVehicle(c *gin.Context) {
+	var createLogVehicle CreateLogVehicleRequest
+	if err := c.ShouldBindJSON(&createLogVehicle); err != nil {
+		log.Println(fmt.Sprintf("error log JoinEvent1: %s", err))
+		c.JSON(http.StatusBadRequest, CreateLogVehicleResponse{
+			Status:  500,
+			Message: "Join Event Failed1",
+		})
+		return
+	}
+
+	createLogVehicleResponse := CreateLogVehicleResponse{
+		Status:  201,
+		Message: "Join Event successfully",
+	}
+
+	createLogVehicleData := vehicle.VehicleMeasurementLogModel{
+		UserId:              createLogVehicle.UserId,
+		VehicleId:           createLogVehicle.VehicleId,
+		MeasurementTitle:    createLogVehicle.MeasurementTitle,
+		CurrentOdo:          createLogVehicle.CurrentOdo,
+		EstimateOdoChanging: createLogVehicle.EstimateOdoChanging,
+		AmountExpenses:      createLogVehicle.AmountExpenses,
+		CheckpointDate:      createLogVehicle.CheckpointDate,
+		Notes:               createLogVehicle.Notes,
+	}
+	// Volunteers Status
+	// 0: idle
+	// 1: accepted
+	// 2: rejected
+	// 3: waiting response
+
+	db := c.MustGet("db").(*gorm.DB)
+	if db.Error != nil {
+		c.JSON(http.StatusBadRequest, CreateLogVehicleResponse{
+			Status:  400,
+			Message: db.Error.Error(),
+		})
+		return
+	}
+
+	check := db.Where("user_id = ?", createLogVehicleData.UserId).Where("vehicle_id = ?", createLogVehicleData.VehicleId).First(&vehicle.VehicleMeasurementLogModel{})
+	log.Println(fmt.Sprintf("error check error: %s", check.Error))
+	log.Println(fmt.Sprintf("error check value: %s", check.Value))
+	if check.Error == nil {
+		c.JSON(http.StatusBadRequest, CreateLogVehicleResponse{
+			Status:  400,
+			Message: "Already Submitted",
+		})
+		return
+	}
+
+	result := db.Create(&createLogVehicleData)
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, CreateLogVehicleResponse{
+			Status:  400,
+			Message: result.Error.Error(),
+		})
+		return
+	}
+	inputNotifModel := notif.Notification{
+		UserId:                  createLogVehicle.UserId,
+		NotificationTitle:       "Add Vehicle",
+		NotificationDescription: "Anda Telah Menambahkan Kendaraan",
+		NotificationStatus:      0,
+		NotificationType:        0,
+	}
+
+	resultNotif := db.Table("notifications").Create(&inputNotifModel)
+	if resultNotif.Error != nil {
+		c.JSON(http.StatusBadRequest, CreateLogVehicleResponse{
+			Status:  400,
+			Message: result.Error.Error() + "Notif error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, createLogVehicleResponse)
+}

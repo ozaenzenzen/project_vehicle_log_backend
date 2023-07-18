79fb2004976a5e30d29a549fe91400f5d9da7a26
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-07-18 21:47:25 +0700

    update: add profile picture in account model

--- models/account/account_model.go
+++ models/account/account_model.go
@@ -5,6 +5,7 @@ import "time"
 type AccountUserModel struct {
 	ID              uint      `json:"id" gorm:"primary_key"`
 	Name            string    `json:"name"`
+	ProfilePicture  string    `json:"profile_picture"`
 	Email           string    `json:"email"`
 	Phone           string    `json:"phone"`
 	Password        string    `json:"password"`

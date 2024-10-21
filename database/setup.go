beeb2d4eea0730e600646c2fa417116318b9ed31
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-10-21 20:19:49 +0700

    update for development

--- database/setup.go
+++ database/setup.go
@@ -14,7 +14,8 @@ func SetupDB() *gorm.DB {
 	HOST := "127.0.0.1"
 	// PORT := "3306"
 	PORT := "3306"
-	DBNAME := "project_vehicle_log_backend" // staging
+	DBNAME := "project_vehicle_log_backend_development" // development
+	// DBNAME := "project_vehicle_log_backend" // staging
 	// DBNAME := "project_vehicle_log_backend2" // development
 	// URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)

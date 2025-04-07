29a8f03132c3853b39f5318e887c027959f88020
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-04-07 13:48:24 +0700

    update setup go

--- database/setup.go
+++ database/setup.go
@@ -46,6 +46,7 @@ func SetupDB() *gorm.DB {
 	// DBNAME := "project_vehicle_log_backend2" // development
 	// URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	fmt.Println("URL: ", URL)
 	db, err := gorm.Open("mysql", URL)
 	if err != nil {
 		panic(err.Error())

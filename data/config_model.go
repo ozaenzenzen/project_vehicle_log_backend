b71d74eefb4dd8478ae5aa7f683b7c43f351b915
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-04-07 12:40:10 +0700

    update some data for db setup

--- data/config_model.go
+++ data/config_model.go
@@ -2,4 +2,9 @@ package data
 
 type Config struct {
 	Secret string `json:"secret"`
+	User   string `json:"user"`
+	Pass   string `json:"pass"`
+	Port   string `json:"port"`
+	DBName string `json:"dbname"`
+	Host   string `json:"host"`
 }
--- database/setup.go
+++ database/setup.go
@@ -1,13 +1,54 @@
 package database
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	configModel "project_vehicle_log_backend/data"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
+func DBConfig() (*string, *string, *string, *string, *string) {
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatalf("Failed to read config file: %v", err)
+		return nil, nil, nil, nil, nil
+	}
+
+	var config configModel.Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		log.Fatalf("Failed to parse config file: %v", err)
+		return nil, nil, nil, nil, nil
+	}
+
+	fmt.Println("Secret:", config.Secret)
+	return &config.User, &config.Pass, &config.Port, &config.DBName, &config.Host
+}
+
 func SetupDB() *gorm.DB {
+	user, pass, port, dbName, host := DBConfig()
+
+	USER := *user
+	PASS := *pass
+	HOST := *host
+	PORT := *port
+	DBNAME := *dbName
+	// DBNAME := "project_vehicle_log_backend" // staging
+	// DBNAME := "project_vehicle_log_backend2" // development
+	// URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	db, err := gorm.Open("mysql", URL)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func SetupDBOld() *gorm.DB {
 	USER := "root"
 	PASS := ""
 	// HOST := "localhost"
@@ -24,5 +65,4 @@ func SetupDB() *gorm.DB {
 		panic(err.Error())
 	}
 	return db
-
 }
